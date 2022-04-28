3c704ce47b25373622619dde9a37f1dd5914acff
Brian Jagodnik <brianjag@mail.mta.ac.il>
2022-04-27 23:47:48 -0300

    WIP

--- App.tsx
+++ App.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default function App() {
+import { API_KEY } from './utils/AccuWeatherAPIKey';
+
+import Forecast from './components/Forecast';
+
+const locationService =
+  'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        getForecast(latitude, longitude);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  });
+
+  const getForecast = (lat: any, lon: any) => {
+    fetch(`${locationService}?q=${lat},${lon}&apikey=${API_KEY}`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {isLoading ? (
+        <Text>Fetching The Weather</Text>
+      ) : (
+        <View>
+          <Text>Pollen Forecast App</Text>
+        </View>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+export default App;
---
+++ utils/AccuWeatherAPIKey.ts
@@ -0,0 +1,1 @@
+export const API_KEY = 'GsqjcHtv0dw4iWTFhD6nNbt3HQGopw3O';
